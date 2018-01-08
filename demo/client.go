11cd7bacfba8395c9bec98ab77ede7da7d6948a1
Russ Magee <rmagee@gmail.com>
2018-01-07 22:05:14 -0800

    Working client/server demos w/HEx and trivial XOR crypto test

--- demo/client.go
+++ demo/client.go
@@ -10,10 +10,14 @@ func main() {
 	conn, err := hkex.Dial("tcp", "localhost:2000")
 	if err != nil {
 		// handle error
+		fmt.Println("Err!")
 	}
-	fmt.Printf("conn: %v\n", conn)
-//		fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
-//		status, err := bufio.NewReader(conn).ReadString('\n')
-//		_, err = bufio.NewReader(conn).ReadString('\n')
+	//fmt.Printf("conn: %v\n", conn)
+
+	fmt.Fprintf(conn, "\x01\x02\x03\x04")
+	//fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	//status, err := bufio.NewReader(conn).ReadString('\n')
+	//_, err = bufio.NewReader(conn).ReadString('\n')
 	// ...
+
 }
--- herradurakex.go
+++ herradurakex.go
@@ -143,6 +143,7 @@ func (h *HerraduraKEx) String() string {
 		h.PeerD.Text(16),
 		h.fa.Text(16))
 }
+
 /*---------------------------------------------------------------------*/
 
 type HKExConn struct {
@@ -158,7 +159,7 @@ func Dial(protocol string, ipport string) (hc *HKExConn, err error) {
 	}
 	hc = &HKExConn{c, New(0, 0)}
 
-	// Stub KEx
+	// KEx
 	fmt.Fprintf(c, "0x%s\n", hc.h.d.Text(16))
 
 	d := big.NewInt(0)
@@ -167,6 +168,7 @@ func Dial(protocol string, ipport string) (hc *HKExConn, err error) {
 		return nil, err
 	}
 	hc.h.PeerD = d
+	fmt.Printf("** D:%s\n", hc.h.d.Text(16))
 	fmt.Printf("**(c)** peerD:%s\n", hc.h.PeerD.Text(16))
 	hc.h.FA()
 	fmt.Printf("**(c)** FA:%s\n", hc.h.fa)
@@ -174,10 +176,11 @@ func Dial(protocol string, ipport string) (hc *HKExConn, err error) {
 }
 
 func (hc *HKExConn) Close() (err error) {
-	err  = hc.c.Close()
+	err = hc.c.Close()
 	fmt.Println("[Conn Closing]")
-        return
+	return
 }
+
 /*---------------------------------------------------------------------*/
 
 type HKExListener struct {
@@ -211,34 +214,45 @@ func (hl *HKExListener) Accept() (hc HKExConn, err error) {
 	d := big.NewInt(0)
 	_, err = fmt.Fscanln(c, d)
 	if err != nil {
+		fmt.Println("[Error]")
 		return hc, err
 	}
 	hc.h.PeerD = d
+	fmt.Printf("** D:%s\n", hc.h.d.Text(16))
 	fmt.Printf("**(s)** peerD:%s\n", hc.h.PeerD.Text(16))
 	hc.h.FA()
 	fmt.Printf("**(s)** FA:%s\n", hc.h.fa)
 
-	// Stub KEx
+	// KEx
 	fmt.Fprintf(c, "0x%s\n", hc.h.d.Text(16))
 
 	return
 }
 
 /*---------------------------------------------------------------------*/
-
 func (hc HKExConn) Read(b []byte) (n int, err error) {
 	n, err = hc.c.Read(b)
-	if n > 0 {
-		fmt.Println("** hc.Read() wraps c.Read() **")
+	fmt.Printf("[Decrypting...]\n")
+	fmt.Printf("[ciphertext:%+v]\n", b[0:n])
+	for i := 0; i < n; i++ {
+		//for i, _ := range b {
+		// FOR TESTING ONLY!! USE REAL CRYPTO HERE
+		//b[i] ^= byte( hc.h.d.Mod(hc.h.d, big.NewInt(int64(c))).Int64() )
+		b[i] ^= hc.h.fa.Bytes()[0]
 	}
+	fmt.Printf("[plaintext:%+v]\n", b[0:n])
 	return
 }
 
 func (hc HKExConn) Write(b []byte) (n int, err error) {
-	n, err = hc.c.Write(b)
-	if n > 0 {
-		//fmt.Printf("** hc.Write('%s') wraps c.Write() **\n", b)
+	fmt.Printf("[Encrypting...]\n")
+	for i, _ := range b {
+		// FOR TESTING ONLY!! USE REAL CRYPTO HERE
+		//b[i] ^= byte( hc.h.d.Mod(hc.h.d, big.NewInt(int64(c))).Int64() )
+		b[i] ^= hc.h.fa.Bytes()[0]
 	}
+	fmt.Printf("[ciphertext:%+v]\n", b)
+	n, err = hc.c.Write(b)
 	return
 }
 
@@ -256,6 +270,7 @@ func NewHKExConn(c *net.Conn) (hc *HKExConn) {
 		//
 	}
 	hc.h.PeerD = d
+	fmt.Printf("** D:%s\n", hc.h.d.Text(16))
 	fmt.Printf("** peerD:%s\n", hc.h.PeerD.Text(16))
 	return
 }
