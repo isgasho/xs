4bccb2512d0f47c7d24dd43fea8d3a9acd7c4111
Russ Magee <rmagee@gmail.com>
2018-01-08 20:24:10 -0800

    Added vanilla client.go server.go for comparison

---
+++ demo/clientp.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:2000")
+	if err != nil {
+		// handle error
+		fmt.Println("Err!")
+	}
+	fmt.Fprintf(conn, "\x01\x02\x03\x04")
+	//fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	//status, err := bufio.NewReader(conn).ReadString('\n')
+	//_, err = bufio.NewReader(conn).ReadString('\n')
+	// ...
+}
