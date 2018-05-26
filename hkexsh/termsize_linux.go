fc1625a0ed975f4c5cc4ccf48ee6e69540cac3d8
Russ Magee <rmagee@gmail.com>
2018-05-26 14:11:27 -0700

    Fixed termsize_linux.go errors and misnamed termsize_windows.go

--- hkexsh/termsize_linux.go
+++ hkexsh/termsize_linux.go
@@ -13,9 +13,6 @@ import (
 
 // Handle pty resizes (notify server side)
 func handleTermResizes(conn *hkexsh.Conn) {
-	rows := 0
-	cols := 0
-
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGWINCH)
 	wg.Add(1)
@@ -25,7 +22,7 @@ func handleTermResizes(conn *hkexsh.Conn) {
 		for range ch {
 			// Query client's term size so we can communicate it to server
 			// pty after interactive session starts
-			cols, rows, err = GetSize()
+			cols, rows, err := GetSize()
 			log.Printf("[rows %v cols %v]\n", rows, cols)
 			if err != nil {
 				log.Println(err)
